0ebdd1eacaf0934ece780b8864226fe11731329a
katy@karstaway.com <katy@karstaway.com>
2018-09-02 11:57:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,7 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if constestant == "Winner"
       # full_name = people["name"]
-      contestant["name"].split.first 
+      contestant["name"].split(" ").first 
         end 
       end
     end
