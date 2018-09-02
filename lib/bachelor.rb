1d3b8f93758952951d6e2337e079de6012688adc
katy@karstaway.com <katy@karstaway.com>
2018-09-02 12:56:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -58,7 +58,7 @@ def get_average_age_for_season(data, season)
      contestant_hash.each do |contestant|
        contestant.each do |contestant_key, contestant_value|
       if contestant_key == "age"
-        age += contestant_value
+        age += contestant_value.to_i
        
       end
     end
