bb572dc995c32ca2c2bc93a5d432e6944656003c
katy@karstaway.com <katy@karstaway.com>
2018-09-02 12:31:19 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,20 +31,22 @@ def count_contestants_by_hometown(data, hometown)
         contestant.each do |k,v|
         if v == hometown
           count += 1
-      
       end
     end
   end
-  
-end
+  end
 return count
 end
-# def get_occupation(data, hometown)
-#   # data.each do |season, contestant_hash|
-#   #   contestant_hash.each do |k,v|
-#   #     if k["hometown"] == hometown
-#   #       contestant.find
-# end
+
+def get_occupation(data, hometown)
+   data.each do |season, contestant_hash|
+     contestant_hash.each do |contestant|
+       contestant.find v == hometown
+         return contestant["occupation"]
+       end
+     end
+   end
+ end
 
 # def get_average_age_for_season(data, season)
 #   # code here
