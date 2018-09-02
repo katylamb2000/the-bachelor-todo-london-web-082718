28a41311f9b524374d0dba096d41684056171385
katy@karstaway.com <katy@karstaway.com>
2018-09-02 12:06:54 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,16 +25,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-   count_hometown = []
+   count = 0
      data.each do |season, contestant_hash|
       contestants.each do |contestant|
-  #   #     if contestant["hometown"] == hometown
-#   #       count_hometown << contestant_hash["name"]
-        
-#   #     end
-#   #     puts count_hometown.length
-#   #   end
-# end
+        if contestant["hometown"] == hometown
+          count += 1
+          count
+        end
+      end
+    end
+  end
 
 # def get_occupation(data, hometown)
 #   # data.each do |season, contestants|
